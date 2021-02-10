525cb77ceea07dc749f0ea57b5ba2b90388ff842
Dima Kashko <kashko_di@mail.ru>
2021-02-10 22:17:12 +0300

    feat: pros

---
+++ src/components/Profile/MyPosts/Post/Post.js
@@ -0,0 +1,9 @@
+import s from './Post.module.css';
+
+const Post = (props) => {
+    return (
+        <div className={s.item}>{props.message}</div>
+    )
+};
+
+export default Post;
---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,21 @@
+import s from './Profile.module.css';
+import MyPosts from "./MyPosts/MyPosts";
+
+const Profile = () => {
+    return (
+        <div className={s.content}>
+            <div>
+                <img className={s.background} src="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg" alt=""/>
+            </div>
+            <div>
+                <img className={s.photo} src="https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg" alt=""/>
+            </div>
+            <div>
+                Main content
+            </div>
+            <MyPosts />
+        </div>
+    )
+}
+
+export default Profile;
